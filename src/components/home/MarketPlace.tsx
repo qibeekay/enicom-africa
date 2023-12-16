f52884801bb0381c3958935e28eaf7cd38b41d57
chibuike mokwe <mokwechibuike7@gmail.com>
2023-12-16 19:17:36 +0100

    store

--- src/components/home/MarketPlace.tsx
+++ src/components/home/MarketPlace.tsx
@@ -1,7 +1,21 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Categories, StoreBaterries } from '..';
 import { useRouter } from 'next/navigation';
+import { getAllProduct } from '@/api/products/products';
+
+// Define types for your data
+interface Product {
+	product_name: string;
+	product_price_th: string;
+	product_image: string;
+	// Add other properties as needed
+}
+
+interface Category {
+	category: string;
+	products: Product[];
+}
 
 const MarketPlace = () => {
 	const router = useRouter();
@@ -9,6 +23,18 @@ const MarketPlace = () => {
 	const handleStore = () => {
 		router.push('/store');
 	};
+	const token = process.env.NEXT_PUBLIC_AUTH_BEARER;
+
+	const [categories, setCategories] = useState<Category[]>([]);
+
+	useEffect(() => {
+		const fetchCategories = async () => {
+			const fetchedCategories = (await getAllProduct(`$${token}`)) || [];
+			setCategories(fetchedCategories);
+		};
+
+		fetchCategories();
+	}, [token]);
 	return (
 		<div className='pt-[50rem] ms:pt-[33rem] mss:pt-[15rem] text-dark font-poppins'>
 			<div>
@@ -26,8 +52,13 @@ const MarketPlace = () => {
 					</div>
 
 					<div className='px-4 lg:px-[4rem]'>
-						<StoreBaterries />
-						<StoreBaterries />
+						{/* Render sliders for all available categories */}
+						{categories.map((category, index) => (
+							<div key={index} className='max-w-6xl mx-auto px-4'>
+								{/* <h1 className='text-xl font-medium'>{category.category}</h1> */}
+								<StoreBaterries category={category.category} />
+							</div>
+						))}
 					</div>
 
 					<div className='grid items-center justify-center'>
